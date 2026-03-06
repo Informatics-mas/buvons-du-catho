b0cbc868737bdd07062b1df9369f387f4cf769b6
informatics-mas <ogouogoudavid@gmail.com>
2026-03-06 21:29:45 +0000

    Fix path and CORS

--- src/backend/server.js
+++ src/backend/server.js
@@ -44,3 +44,6 @@ app.use("/api/produits", produitStandRoutes);
 // 6. DÉMARRAGE DU SERVEUR
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
+
+const cors = require('cors');
+app.use(cors()); // Cela permet à ton localhost:3000 de parler à Render sans blocage
