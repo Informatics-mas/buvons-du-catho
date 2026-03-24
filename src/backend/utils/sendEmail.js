1b5a01b3943a7bf0ccef935cccdb9719ac662784
informatics-mas <ogouogoudavid@gmail.com>
2026-03-24 18:42:06 +0000

    feat: ajout du temps réel pour les réservations 5.5

--- src/backend/utils/sendEmail.js
+++ src/backend/utils/sendEmail.js
@@ -13,13 +13,19 @@ console.log("DEBUG DANS SENDEMAIL - USER :", process.env.EMAIL_USER);
 
 // Configuration du transporteur
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // Utilise SSL
+  host: "smtp.gmail.com", // Ou ton hôte actuel
+  port: 587,              // 👈 Change 465 pour 587
+  secure: false,          // 👈 Doit être 'false' pour le port 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    // 👈 Force la connexion à ne pas échouer sur les réseaux restreints
+    rejectUnauthorized: false,
+    // 👈 Optionnel : Force IPv4 si possible
+    servername: "smtp.gmail.com" 
+  }
 });
 
 // Vérification de la connexion au démarrage (utile pour le débug sur Render)
